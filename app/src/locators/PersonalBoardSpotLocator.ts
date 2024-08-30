d8dc8feae82a1e060edc42daf75c73a97d9affde
Romain Fromi <romain.fromi@gmail.com>
2024-08-30 16:42:50 +0200

    Display personal boards for 4 player games

--- app/src/locators/PersonalBoardSpotLocator.ts
+++ app/src/locators/PersonalBoardSpotLocator.ts
@@ -1,9 +1,22 @@
-import { Locator, MaterialContext } from '@gamepark/react-game'
+import { getRelativePlayerIndex, Locator, MaterialContext } from '@gamepark/react-game'
 import { Location } from '@gamepark/rules-api'
 
 class PersonalBoardSpotLocator extends Locator {
   // player = rules.players[0] => spectator default point of view
-  getCoordinates(location: Location, { rules, player = rules.players[0] }: MaterialContext) {
+  getCoordinates(location: Location, context: MaterialContext) {
+    const { rules, player = rules.players[0] } = context
+    if (rules.players.length === 4) {
+      switch (getRelativePlayerIndex(context, location.player)) {
+        case 0:
+          return { y: 25 }
+        case 1:
+          return { x: -25 }
+        case 2:
+          return { y: -25 }
+        case 3:
+          return { x: 25 }
+      }
+    }
     if (location.player === player) {
       return { y: 25 }
     } else {
