e4b9441651908c932b8ad6800dfb052ed2ce3e21
Romain Fromi <romain.fromi@gmail.com>
2024-08-30 10:42:55 +0200

    Prepare the location types

--- rules/src/material/LocationType.ts
+++ rules/src/material/LocationType.ts
@@ -1,2 +1,13 @@
 export enum LocationType {
+  PersonalBoardSpot = 1, // Spot on the table
+  Stage, // Stage on a personal board
+  PersonalWaitingZone, // Zone on a personal board
+  SideWaitingZoneSpot, // Spot on the table
+  CentralWaitingZoneSpot, // Spot on the table
+  SideWaitingZone, // On a side waiting zone
+  CentralWaitingZone, // On the central waiting zone
+  FigureSpace, // Space on a personal board
+  ScoreboardSpace, // Space on a personal board
+  ScoringTokensStock, // Stock on the table
+  Scoreboard // Scoring spot on a scoreboard
 }
