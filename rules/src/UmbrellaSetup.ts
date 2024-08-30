360c2417bcbae8ef8f4d7db651ef8ca934dd8d81
Romain Fromi <romain.fromi@gmail.com>
2024-08-30 17:27:57 +0200

    Init strategy for describing players stage setup nicely

--- rules/src/UmbrellaSetup.ts
+++ rules/src/UmbrellaSetup.ts
@@ -1,7 +1,7 @@
 import { MaterialGameSetup } from '@gamepark/rules-api'
 import { LocationType } from './material/LocationType'
 import { MaterialType } from './material/MaterialType'
-import { Umbrella } from './material/Umbrella'
+import { Umbrella, umbrellas } from './material/Umbrella'
 import { PlayerColor } from './PlayerColor'
 import { RuleId } from './rules/RuleId'
 import { UmbrellaOptions } from './UmbrellaOptions'
@@ -21,7 +21,10 @@ export class UmbrellaSetup extends MaterialGameSetup<PlayerColor, MaterialType, 
     for (const player of this.game.players) {
       for (let x = 0; x < 4; x++) {
         for (let y = 0; y < 4; y++) {
-          this.material(MaterialType.UmbrellaToken).createItem({ id: Umbrella.Red, location: { type: LocationType.Stage, player, x, y } })
+          this.material(MaterialType.UmbrellaToken).createItem({
+            id: stagesSetup[player][y][x],
+            location: { type: LocationType.Stage, player, x, y }
+          })
         }
       }
     }
@@ -30,4 +33,33 @@ export class UmbrellaSetup extends MaterialGameSetup<PlayerColor, MaterialType, 
   start() {
     this.startPlayerTurn(RuleId.SlideUmbrella, this.game.players[0])
   }
+}
+
+const [R, B, G, Y] = umbrellas
+
+const stagesSetup: Record<PlayerColor, Umbrella[][]> = {
+  [PlayerColor.Red]: [
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y]
+  ],
+  [PlayerColor.Green]: [
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y]
+  ],
+  [PlayerColor.Yellow]: [
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y]
+  ],
+  [PlayerColor.Pink]: [
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y],
+    [R, B, G, Y]
+  ]
 }
